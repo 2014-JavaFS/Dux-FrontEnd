f4417e4c57e7744e3cb21806dc2ba58266a68e03
Luis-Cruz1027 <luisalejandrocruz1027@gmail.com>
2024-08-07 13:36:38 -0500

    front end of register implemented. as of right now on successful registration, user is taken to '/' endpoint (main)

--- dux-frontend/src/main.tsx
+++ dux-frontend/src/main.tsx
@@ -1,12 +1,12 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import App from './App.tsx'
-import DuckList from "./components/duck-list.tsx"
-import GetCart from "./components/get-cart.tsx"
-import User from './components/user-info.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App.tsx";
+import DuckList from "./components/duck-list.tsx";
+import GetCart from "./components/get-cart.tsx";
+import Register from './components/register.tsx';
+import User from './components/user-info.tsx';
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -18,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path="/ducks" element={<DuckList />} />
         <Route path="/cart" element={<GetCart userId={2} />} />
+        <Route path='/users/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
